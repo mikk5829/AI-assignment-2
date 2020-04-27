479944dfda045d40fc0a361b737abaac7c632e6c
Mikkel Anderson <s184230@student.dtu.dk>
2020-04-27 22:28:42 +0200

    Project set up

--- AI-assigntment-2/compile.sh
+++ AI-assigntment-2/compile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 mono FsLexYacc.10.0.0/build/fslex/net46/fslex.exe Lexer.fsl --unicode
-mono FsLexYacc.10.0.0/build/fslex/net46/fslex.exe SignAnalysisLexer.fsl --unicode
-mono FsLexYacc.10.0.0/build/fslex/net46/fslex.exe SecurityAnalysisLexer.fsl --unicode
 
 mono FsLexYacc.10.0.0/build/fsyacc/net46/fsyacc.exe Parser.fsp --module Parser
-mono FsLexYacc.10.0.0/build/fsyacc/net46/fsyacc.exe SignAnalysisParser.fsp --module SignAnalysisParser
-mono FsLexYacc.10.0.0/build/fsyacc/net46/fsyacc.exe SecurityAnalysisParser.fsp --module SecurityAnalysisParser
